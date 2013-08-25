b60eba77d3a306dc0276edcbcd154a0a2e7c3599
jm2dev <jomarcar@gmail.com>
2013-08-25 19:32:39 +0100

    Service host and port configurable, now localhost:9090.

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -4,7 +4,7 @@ import akka.actor.{ActorSystem, Props}
 import akka.io.IO
 import spray.can.Http
 
-object Boot extends App {
+object Boot extends App with Configuration {
 
   // we need an ActorSystem to host our application in
   implicit val system = ActorSystem("on-spray-can")
@@ -13,5 +13,5 @@ object Boot extends App {
   val service = system.actorOf(Props[MyServiceActor], "demo-service")
 
   // start a new HTTP server on port 8080 with our service actor as the handler
-  IO(Http) ! Http.Bind(service, interface = "localhost", port = 8080)
+  IO(Http) ! Http.Bind(service, interface = serviceHost, port = servicePort)
 }
---
+++ src/main/scala/Configuration.scala
@@ -0,0 +1,21 @@
+package com.jm2dev
+
+import com.typesafe.config.ConfigFactory
+import util.Try
+
+/**
+* Holds service configuration settings.
+*/
+trait Configuration {
+
+  /**
+* Application config object.
+*/
+  val config = ConfigFactory.load()
+
+  /** Host name/address to start service on. */
+  lazy val serviceHost = Try(config.getString("service.host")).getOrElse("localhost")
+
+  /** Port to start service on. */
+  lazy val servicePort = Try(config.getInt("service.port")).getOrElse(8080)
+}
